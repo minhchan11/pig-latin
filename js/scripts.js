e4880fca52a86b0ef8766921efdce5673ae289a3
Minh <mphuong@kent.edu>
2017-01-18 21:16:55 -0800

    insert to write logic for first character

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,60 @@
 //BUSINESS LOGIC:
-var userInput = 0;
-var stringChar = [];
-var vowels = ["a","e","i","o","u"];
-var outputString = 0;
+var stringChar1 = 0;
+var stringChar2 = 0;
+var vowels = ["a","e","i","o","u","A","I","E","O","U"];
 var sentence = 0;
 var eachword = 0;
-var newwords = [];
-var letter = [];
-var j = 0;
+var result1 = 0
+
 
 function verify() {
-  for (var j = 0; j <= newwords.length+3; j++) {
-    console.log(newwords.length);
-    newwords.map(function(newword) {
-      letter.push(newword[j].split(""));
-      console.log(letter);
-    });
-    for (var index = 0; index < vowels.length; index++) {
-          console.log(letter[j]);
-          if ((letter[j][0]) === vowels[index]) {
-            console.log("first vowel");
-            alert("vowels");
-          } else {
-              if ((letter[j][j] === "q") && (letter[j][j+1] === vowels[4])) {
-                console.log(" q u");
-                alert("vowels");
-              } else if (letter[j][1] !== vowels[index]) {
-                console.log("hooray");
-                alert("vowels");
-              }
-            }
+    eachword= sentence.split(" ");
+    for (var j = 0; j < eachword.length; j++) {
+    stringChar1 = eachword[j].charAt(0);
+    stringChar2 = eachword[j].charAt(1);
+      for (var i = 0; i < vowels.length; i++) {
+        if (stringChar1 === vowels[i]) {
+          result1 = eachword[j].concat("ay");
+          console.log(result1);
+    } else {
+      if (stringChar2 !== )
+    result2 = (eachword[j].replace(stringChar1,"")).concat(stringChar1+"ay")
+    console.log(result2);
+  }
+}
 
-    }
-  };
+}
+}
+    // for (var i = 0; i < vowels.length; i++) {
+    //       // if (stringChar === vowels[i]) {
+    //         console.log(stringChar);
+            // console.log(vowels[i]);
+    //         console.log("vowel");
+    //         alert("vowels");
+    //       } else {
+    //         return false;
+    // //           if ((letter[j][j] === "q") && (letter[j][j+1] === vowels[4])) {
+    // //             console.log(" q u");
+    // //             alert("vowels");
+    // //           } else if (letter[j][1] !== vowels[index]) {
+    // //             console.log("hooray");
+    // //             alert("vowels");
+    //           }
+            // }
+
+    // }
+
 
 
 //     outputString=stringChar.join("");
-};
+// };
 // };
 
 //USER INTERFACE:
 $(document).ready(function() {
-  $(".form-group").submit(function(event) {
+  $("form").submit(function(event) {
     event.preventDefault();
     sentence = $("#usertext").val();
-    eachword= sentence.split(" ");
-    newwords.push(eachword);
-    console.log(newwords)
-    verify(letter);
+    verify();
   });
 });
