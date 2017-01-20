adb537a093f2b38ee6dd590123c6debee5c708c5
Minh <mphuong@kent.edu>
2017-01-19 23:04:04 -0800

    fixed loops for qu and consonant+qu

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 //BUSINESS LOGIC:
 var stringChar1 = 0;
 var stringChar2 = 0;
+var stringChar3 = 0;
 var vowels = ["a","e","i","o","u","A","I","E","O","U"];
 var sentence = 0;
 var eachword = 0;
@@ -10,22 +11,18 @@ function verify() {
     for (var j = 0; j < eachword.length; j++) {
     stringChar1 = eachword[j].charAt(0);
     stringChar2 = eachword[j].charAt(1);
+    stringChar3 = eachword[j].charAt(2);
         if (vowels.indexOf(stringChar1) >-1 ) {
-          result1 = eachword[j].concat("ay");
-          console.log(result1);
+          result = eachword[j].concat("ay");
         } else {
-          //   if ((vowels.indexOf(stringChar1) === -1) && (vowels.indexOf(stringChar2) === -1 )) {
-          //   result3 = (eachword[j].slice(0,2)).concat(stringChar1+stringChar2+"ay")
-          //   console.log(result3);
-          // }
-          // else {
-          if (vowels.indexOf(stringChar1) === -1 && vowels.indexOf(stringChar2) === -1 ) {
-              result3 = (eachword[j].slice(2)).concat(stringChar1+stringChar2+"ay")
-              console.log(result3);
-              }
-          else if ( vowels.indexOf(stringChar1) === -1 )
-          { result2 = (eachword[j].replace(stringChar1,"")).concat(stringChar1+"ay");
-          console.log(result2); }
+          if (vowels.indexOf(stringChar1) === -1 && (stringChar2 === "q"||stringChar2 === "u")) {
+            result = (eachword[j].slice(3)).concat(stringChar1+"quay")
+          } else if (stringChar1 === "q" && stringChar2 === "u") {
+            result = (eachword[j].slice(2)).concat("quay")
+          } else if (vowels.indexOf(stringChar1) === -1 && vowels.indexOf(stringChar2) === -1 ) {
+              result = (eachword[j].slice(2)).concat(stringChar1+stringChar2+"ay")
+          } else { result = (eachword[j].replace(stringChar1,"")).concat(stringChar1+"ay");
+          }
 
 
 }
